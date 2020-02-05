2fabb1ceaaa766aa7f0b2a7b1502ccf2214af5d7
gabrielVargas611 <gavargas77@gmail.com>
2020-02-05 15:58:41 -0600

    Model & Interface

---
+++ Contract/IMovement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WalletAPI.Models;
+
+namespace WalletAPI.Contract
+{
+    interface IMovement
+    {
+        Movement Create(Movement movement);
+
+        List<Movement> Get();
+
+        Movement Get(int id);
+
+        void Remove(int id);
+
+        //Pregunatr por este, el por q existe, creo q es para borrar un seleionado
+        //void remove(int id, Movement moventIn);
+
+        //Preguntar por q 2 aqui
+        void Upgrade(string id, Movement movementIn);
+    }
+}
---
+++ Models/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalletAPI.Models
+{
+    public class Movement
+    {
+       private int Id { get; set; }
+
+       private string Name { get; set; }
+
+        private string Category { get; set; }
+
+        private decimal Ammount { get; set; }
+
+        private DateTime DateTime { get; set; }
+
+        private Boolean Type { get; set; }
+    }
+}
